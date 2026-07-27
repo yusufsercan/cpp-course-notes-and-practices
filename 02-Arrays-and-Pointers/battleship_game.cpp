799e18ba0c8f23830bb3cd1442985aa82034196d
yusufsercan08 <yusufsercanisci2005@gmail.com>
2026-07-27 14:18:23 +0300

    updated

--- 02-Arrays-and-Pointers/battleship_game.cpp
+++ 02-Arrays-and-Pointers/battleship_game.cpp
@@ -14,13 +14,12 @@
 
 
 
-
-
 using namespace std;
 
 // Basit düzeyde amiral battı oyunu (Battleship)
 int main() {
-    bool ships[4][4] = {
+    bool ships[4][4] = 
+    {
         {0, 0, 1, 1},
         {0, 0, 0, 0},
         {0, 0, 0, 1},
@@ -31,7 +30,8 @@ int main() {
     int numberOfTurns = 0;
     int row = 0, col = 0;
 
-    while (hits < 4) {
+    while (hits < 4) 
+    {
         cout << "\nSelecting coordinates...\n";
         cout << "Choose a row number between 0 and 3: ";
         cin >> row;
@@ -40,17 +40,21 @@ int main() {
         cin >> col;
 
         // GÜVENLİK KONTROLÜ: Her iki girdi de alındıktan sonra kontrol edilir
-        if (row < 0 || row > 3 || col < 0 || col > 3) {
+        if (row < 0 || row > 3 || col < 0 || col > 3)
+        {
             cout << "Gecersiz koordinat! Lutfen 0 ile 3 arasinda sayilar girin.\n";
             continue; // Hatalı girdi olduğu için hamle sayısını artırmadan döngünün başına dön
         }
 
         // Vurulan konumda gemi var mı kontrol edelim
-        if (ships[row][col]) {
+        if (ships[row][col]) 
+        {
             ships[row][col] = 0; 
             hits++;             
             cout << "You hit! (" << (4 - hits) << " left)\n";
-        } else {
+        } 
+        else 
+        {
             cout << "You missed!\n";
         }
 
