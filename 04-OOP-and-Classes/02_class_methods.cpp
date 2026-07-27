da8ec2d589dce9fadcd3269364e9a53738ff8906
yusufsercan08 <yusufsercanisci2005@gmail.com>
2026-07-27 17:47:38 +0300

    Add 02_class_methods.cpp demonstrating class methods and parameters

---
+++ 04-OOP-and-Classes/02_class_methods.cpp
@@ -0,0 +1,47 @@
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Nesne yönelimli programlamada fonksiyon (Method) kullanımı
+class Car
+{
+public:
+    // Attributes (Özellikler)
+    string brand; // brand: marka
+    string model;
+    int year;
+
+    // Methods (Davranışlar / Fonksiyonlar)
+    void message()
+    {
+        cout << "Hello C++ OOP!\n";
+    }
+s
+    int speed(int mySpeed)
+    {
+        return mySpeed;
+    }
+};
+
+int main()
+{
+    Car c1;
+    c1.brand = "Skoda";
+    c1.model = "Superb";
+    c1.year = 2001;
+
+    Car c2;
+    c2.brand = "Nissan";
+    c2.model = "Juke";
+    c2.year = 2003;
+
+    c1.message();
+    
+    cout << c1.brand << "-" << c1.model << "-" << c1.year << "-" << c1.speed(180) << "\n";
+    cout << c2.brand << "-" << c2.model << "-" << c2.year << "-" << c2.speed(140) << "\n";
+
+    return 0;
+}
--- 04-OOP-and-Classes/README.md
+++ 04-OOP-and-Classes/README.md
@@ -8,3 +8,4 @@ This directory covers Object-Oriented Programming (OOP) concepts in C++, includi
 ### 📂 Code References
 
 * **`01_class_and_object_basics.cpp`**: Core concepts of classes, object instantiation, access specifiers (`public`), and attribute isolation in RAM.
+* **`02_class_methods.cpp`**: Demonstrates class member functions (methods), method invocation, and passing parameters within an object context.
